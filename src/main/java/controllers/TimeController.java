57515d7e952cb5c29d8a286190d9cee4ae98b032
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-06-17 20:30:06 +0700

    Thuc hanh hien thi thoi gian

---
+++ src/main/java/controllers/TimeController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+    @GetMapping("/worldclock")
+    public String getByTimeZone(
+            ModelMap modelMap,
+            @RequestParam(name = "city",required = false,defaultValue = "Asia/Ho_Chi_Minh")String city
+    )
+    {
+        Date date = new Date();
+        TimeZone local = TimeZone.getDefault();
+        TimeZone localate = TimeZone.getTimeZone(city);
+        long locale_time = date.getTime()+(localate.getRawOffset()-local.getRawOffset());
+        date.setTime(locale_time);
+        modelMap.addAttribute("city",city);
+        modelMap.addAttribute("date",date);
+
+        return "index";
+    }
+}
